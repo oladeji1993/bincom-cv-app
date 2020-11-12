b3b32a6d341f37abe363512cfbfacb107b37f673
oladeji1993 <olanipekunoladeji282@yahoo.com>
2020-11-12 01:56:02 +0100

    first commit

---
+++ www/js/controller.js
@@ -0,0 +1,28 @@
+document.querySelector('.menu-btn').addEventListener
+('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+
+document.querySelector('.mini').addEventListener
+('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+
+document.querySelector('.remove').addEventListener
+('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+
+document.querySelector('.removeEducation').addEventListener
+('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+
+document.querySelector('.removeSkill').addEventListener
+('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+
+document.querySelector('.details').addEventListener
+('click', () => document.querySelector('.main-menu').classList.toggle('show'));
+
+
+
+
+
+
+
+
+
+
+
